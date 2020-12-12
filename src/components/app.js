f0770acea99c593731911e5c810ac84462bf47a2
Aniz KA <aniz.ka@team.telstra.com>
2020-12-12 22:38:23 +1100

    structured the project

---
+++ src/components/app.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Heading from "./Heading";
+import Greeting from "./Greeting";
+import List from "./List";
+
+const num = 8;
+
+const customStyle = {
+    color: "blue",
+    fontSize: "20px",
+    border: "1px solid black"
+  };
+
+function App() {
+    return(
+    <div>
+    <Heading /> 
+    <Greeting />
+    <List />
+    <p style={customStyle}>My lucky number is {Math.random() * num * 10}</p>
+    <p> Created by Aniz</p>
+  </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Heading from "./Heading";
-import List from "./List";
-import Greeting from "./Greeting";
+import App from "./components/app";
 
-const num = 8;
-const customStyle = {
-  color: "blue",
-  fontSize: "20px",
-  border: "1px solid black"
-};
-
-ReactDOM.render(
-  <div>
-    <Heading /> <Greeting />
-    <List />
-    <p style={customStyle}>My lucky number is {Math.random() * num * 10}</p>
-    <p> Created by Aniz</p>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />,document.getElementById("root"));
